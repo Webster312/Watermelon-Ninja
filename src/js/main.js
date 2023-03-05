a4ea8ad9ee1b6cdcfcb26bba9287bc51e49ddbbe
Christoph Weber <WeberChristoph85@gmail.com>
2023-03-05 11:09:14 +0100

    added and adjusted velocity and number of 'watermelons' appearing on the canvas

--- src/js/main.js
+++ src/js/main.js
@@ -3,29 +3,71 @@
 window.onload = function () {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
+    const GRAVITY = 0.2;
     
-    function drawCircle(x, y) {
+    const circles = [];
+
+
+    // creating the circles 'watermelon'
+    function drawCircle(x, y, size) {
       ctx.beginPath();
-      ctx.arc(x, y, 10, 0, Math.PI * 2, false);
+      ctx.arc(x, y, size, 0, Math.PI * 2, false);
       ctx.fillStyle = "green";
       ctx.fill();
     }
     
+    // clearing the canvas
     function clearCanvas() {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
     
+    // generating random circles
     function randomInt(min, max) {
       return Math.floor(Math.random() * (max - min + 1) + min);
     }
     
-    function animate() {
-      clearCanvas();
-      const x = randomInt(0, canvas.width);
-      const y = randomInt(0, canvas.height);
-      drawCircle(x, y);
-      setTimeout(animate, 2000);
+    // animating the circles
+   function createCircle() {
+    const x = randomInt(0, canvas.width);
+    const y = canvas.height;
+    const r = randomInt(20, 20);
+    const vx = randomInt(-5, 5.5);
+    const vy = randomInt(-10, -4.5);
+    circles.push({x, y, r, vx, vy});
+  }
+
+  function updateCircle(circle) {
+    circle.x += circle.vx;
+    circle.y += circle.vy;
+    circle.vy += GRAVITY;
+  }
+
+  function animate() {
+    clearCanvas();
+    
+    if (circles.length < 5) {       // number of animated circles
+      createCircle();
     }
     
-    animate();
+    circles.forEach(circle => {
+      updateCircle(circle);
+      if (circle.y > canvas.height) {
+        const index = circles.indexOf(circle);
+        circles.splice(index, 1);
+      } else {
+        drawCircle(circle.x, circle.y, circle.r);
+      }
+    });
+    
+    requestAnimationFrame(animate);
+  }
+
+  animate();
 }
+
+
+
+
+
+
+  
